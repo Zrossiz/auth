d0a33ca9282bed65045d7670a1b1d386f4f96955
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 13:54:21 +0300

    feat: added create findOne & delete user routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('')
+  createUser(@Body() dto) {
+    return this.userService.save(dto);
+  }
+
+  @Get(':idOrEmail')
+  findOneUser(@Param() idOrEmail: string) {
+    return this.userService.findOne(idOrEmail);
+  }
+
+  @Delete(':id')
+  delete(@Param() id: string) {
+    return this.userService.delete(id);
+  }
+}
