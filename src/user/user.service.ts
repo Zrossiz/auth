b14efe6d384e78a99ea6aba183d9977de4b6835b
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 13:41:19 +0300

    feat: added method save

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async save(user: Partial<User>) {
+    return await this.prismaService.user.create({
+      data: {
+        email: user.email,
+        password: user.password,
+        roles: ['User'],
+      },
+    });
+  }
+
+  findOne() {}
+
+  delete() {}
+}
