ccba1fcc3e2a7f56e5735eb25a859b82ad8b50e7
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 14:32:45 +0300

    feat: jwt module async options logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { options } from './config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [PassportModule, JwtModule.registerAsync(), UserModule],
+  imports: [PassportModule, JwtModule.registerAsync(options()), UserModule],
 })
 export class AuthModule {}
--- src/auth/config/jwt-module-async-options.ts
+++ src/auth/config/jwt-module-async-options.ts
@@ -1,6 +1,14 @@
-import { JwtModuleAsyncOptions } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtModuleAsyncOptions, JwtModuleOptions } from '@nestjs/jwt';
+
+const JwtModuleOptions = (config: ConfigService): JwtModuleOptions => ({
+  secret: config.get('JWT_SECRET'),
+  signOptions: {
+    expiresIn: config.get('JWT_EXP', '5m'),
+  },
+});
 
 export const options = (): JwtModuleAsyncOptions => ({
-  inject: [],
-  useFactory: () => {},
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => JwtModuleOptions(config),
 });
