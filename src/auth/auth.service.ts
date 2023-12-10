5879689748eff79c4e15bd7cbe231ff5f0478296
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 16:11:37 +0300

    feat: added tokens interfaces and auth register method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { LoginDto, RegisterDto } from './dto';
+import { UserService } from 'src/user/user.service';
+import { Tokens } from './interafaces';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
+  constructor(private readonly userService: UserService) {}
+
+  async register(dto: RegisterDto) {
+    return this.userService.save(dto).catch((err) => {
+      this.logger.error(err);
+      return null;
+    });
+  }
+
+  async login(dto: LoginDto): Promise<Tokens> {
+    const user = await this.userService.findOne(dto.email).catch((err) => {
+      this.logger.error(err);
+      return null;
+    });
+    return;
+  }
+}
