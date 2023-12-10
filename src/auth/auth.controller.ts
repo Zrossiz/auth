1c49949b08fae4f0ebf1ae26334e79ae1ea82083
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 14:37:32 +0300

    feat: added register login refresh routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('register')
+  register(@Body() dto) {}
+
+  @Post('login')
+  login(@Body() dto) {}
+
+  @Get('refresh')
+  refreshToken() {}
+}
