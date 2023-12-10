8163c6e014dbb9f0a4fecf384d3ebf204bf8236b
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 16:39:16 +0300

    feat: added register and login routes (auth)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,38 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { LoginDto, RegisterDto } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('register')
-  register(@Body() dto: RegisterDto) {}
+  async register(@Body() dto: RegisterDto) {
+    const user = await this.authService.register(dto);
+
+    if (!user) {
+      throw new BadRequestException(
+        `Ошибка при регистрации пользователя с данными ${JSON.stringify(dto)}`,
+      );
+    }
+  }
 
   @Post('login')
-  login(@Body() dto: LoginDto) {}
+  async login(@Body() dto: LoginDto) {
+    const tokens = await this.authService.login(dto);
+
+    if (!tokens) {
+      throw new BadRequestException(
+        `Ошибка при входе с данными ${JSON.stringify(dto)}`,
+      );
+    }
+  }
 
   @Get('refresh')
   refreshToken() {}
