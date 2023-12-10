f1cde263ba95c91583fcb4ab3e54577cd98bcd59
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 18:07:34 +0300

    feat: added cookie-parser

--- libs/common/src/decorators/cookie.decorator.ts
+++ libs/common/src/decorators/cookie.decorator.ts
@@ -3,6 +3,6 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 export const Cookie = createParamDecorator(
   (key: string, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
-    return key ? req.cookies[key] : req.cookies;
+    return key && key in req.cookies ? req.cookies[key] : req.cookies;
   },
 );
