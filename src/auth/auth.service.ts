e1ccf36efa11c0d2590a3b487361397f72b08783
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-12-10 16:19:45 +0300

    feat: added accessToken and getRefreshToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,22 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { LoginDto, RegisterDto } from './dto';
 import { UserService } from 'src/user/user.service';
 import { Tokens } from './interafaces';
+import { compareSync } from 'bcrypt';
+import { Token, User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { v4 } from 'uuid';
+import { add } from 'date-fns';
 
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger(AuthService.name);
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   async register(dto: RegisterDto) {
     return this.userService.save(dto).catch((err) => {
@@ -16,10 +26,30 @@ export class AuthService {
   }
 
   async login(dto: LoginDto): Promise<Tokens> {
-    const user = await this.userService.findOne(dto.email).catch((err) => {
-      this.logger.error(err);
-      return null;
+    const user: User = await this.userService
+      .findOne(dto.email)
+      .catch((err) => {
+        this.logger.error(err);
+        return null;
+      });
+    if (!user || !compareSync(dto.password, user.password)) {
+      throw new UnauthorizedException('Неверный логин или пароль');
+    }
+
+    const accessToken = this.jwtService.sign({
+      id: user.id,
+      email: user.email,
+      roles: user.roles,
     });
-    return;
+  }
+
+  private async getRefreshToken(userId: string): Promise<Token> {
+    return this.prismaService.token.create({
+      data: {
+        token: v4(),
+        exp: add(new Date(), { months: 1 }),
+        userId,
+      },
+    });
   }
 }
